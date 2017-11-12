0fb9564fc5308e6a1f34a5e9f0cd9ab23ae0a27f
Jube Schi <jubeschi@gmail.com>
2017-11-12 20:41:12 +0100

    added failing unit test

--- functional_test.py
+++ functional_test.py
@@ -1,9 +1,22 @@
 from selenium import webdriver
-import re
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000/dashapp')
+class NewMeasure(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.browser.quit()
 
-#assert 'dashapp' in browser.find_elements(by, value)
-body = browser.find_element_by_tag_name("body")
-assert 'dashapp' in body.text
+    def test_can_enter_a_measure(self):
+        #User can enter a new measure
+        self.browser.get('http://localhost:8000/dashapp')
+
+        #assert 'dashapp' in browser.find_elements(by, value)
+        body = self.browser.find_element_by_tag_name("body")
+        self.assert_('dashapp' in body.text, "Did not find dashall in Body")
+        self.fail('Finish the test!')
+        
+if __name__ == '__main__':
+    unittest.main()
