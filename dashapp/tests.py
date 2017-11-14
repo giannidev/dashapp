e4a98d44b0476e49b574158c0914a63b1b5d3e12
Jube Schi <jubeschi@gmail.com>
2017-11-14 21:20:58 +0100

    basic view now returns html

--- dashapp/tests.py
+++ dashapp/tests.py
@@ -3,10 +3,17 @@ from __future__ import unicode_literals
 from django.test import TestCase
 from django.urls import resolve
 from dashapp.views import home_page
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
     
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
+        
+    def test_assert_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
         
--- dashapp/views.py
+++ dashapp/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
+
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the dashapp index.")
