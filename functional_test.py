959f2252e2e74ffa2c94acf58c2095664a07ce95
giannidev <gianni.pucciani@gmail.com>
2017-11-19 22:27:54 +0100

    added two measures, retrieving only one

--- functional_test.py
+++ functional_test.py
@@ -11,20 +11,8 @@ class NewMeasure(unittest.TestCase):
         
     def tearDown(self):
         self.browser.quit()
-
-    def test_can_enter_a_measure(self):
-        #Gianni wants to enter a new measure
         
-        #He goes to his brand new webapp 
-        self.browser.get('http://localhost:8000/')
-
-        #He sees the message inviting to enter a new measure
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        #body = self.browser.find_element_by_tag_name("body")
-        #self.assert('dashapp' in body.text, "Did not find dashall in Body")
-        self.assertIn(header_text,'Enter a new measure')
-        
-        #He is invited to enter new measure by entering a metric and a measure
+    def inputMeasure(self, metric, measure):
         inputMetric = self.browser.find_element_by_id('id_new_metric')
         self.assertEqual(
             inputMetric.get_attribute('placeholder'),
@@ -45,11 +33,29 @@ class NewMeasure(unittest.TestCase):
         
         #When he hits enter the values are stored and showed in the recently entered list
         inputMetric.send_keys(Keys.ENTER)  
-        time.sleep(5)
+
+    def test_can_enter_a_measure(self):
+        #Gianni wants to enter a new measure
+        
+        #He goes to his brand new webapp 
+        self.browser.get('http://localhost:8000/')
+
+        #He sees the message inviting to enter a new measure
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        #body = self.browser.find_element_by_tag_name("body")
+        #self.assert('dashapp' in body.text, "Did not find dashall in Body")
+        self.assertIn(header_text,'Enter a new measure')
+        
+        #He enters two measures
+        self.inputMeasure('M1', '10')
+        time.sleep(2)
+        self.inputMeasure('M2', '20')
+        time.sleep(2)
         
         recentTable = self.browser.find_element_by_id('id_recent_table')  
         rows = recentTable.find_elements_by_tag_name('tr')
         self.assertIn('M1: 10', [row.text for row in rows])
+        self.assertIn('M2: 20', [row.text for row in rows] )
         #self.assertTrue(
          #   any(row.text == 'M1: 10' for row in rows),
          # f"New measure did not appear in table. Contents were:\n{recentTable.text}"            )
