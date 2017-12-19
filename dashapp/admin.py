4ea1cc93a812a6f150d10066865de43f03af119d
giannidev <gianni.pucciani@gmail.com>
2017-12-19 21:29:13 +0100

    custom admin interface

--- dashapp/admin.py
+++ dashapp/admin.py
@@ -7,6 +7,24 @@ from .models import Metric
 from .models import Measure
 from .models import Event
 
-admin.site.register(Metric)
-admin.site.register(Measure)
-admin.site.register(Event)
+class EventAdmin (admin.ModelAdmin):
+    list_display = ('name','date')
+    
+class MetricAdmin (admin.ModelAdmin):
+    list_display = ('name','description','frequency','modifier')
+    #list_filter = ('frequency')
+    
+class MeasureAdmin (admin.ModelAdmin):
+    list_display = ('get_metric_name','get_event_name','rawValue','value')
+    
+    def get_event_name(self,obj):
+        return obj.event.name
+    def get_metric_name(self,obj):
+        return obj.metric.name
+    def get_metric_description(self,obj):
+        return obj.metric.description
+
+
+admin.site.register(Metric, MetricAdmin)
+admin.site.register(Measure, MeasureAdmin)
+admin.site.register(Event, EventAdmin)
